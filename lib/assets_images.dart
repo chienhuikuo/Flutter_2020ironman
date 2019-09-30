cd5002439fc97087bea07a24e0ff29873aede8b9
chienhuikuo <s110019038@gmail.com>
2019-09-30 22:07:47 +0800

    assets image

---
+++ lib/assets_images.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(new MyApp());
+} 
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(  
+      title: 'hello',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Succulent'),
+          backgroundColor: Color.fromRGBO(250, 205, 75, 1.0),
+        ),
+        body: Center(
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              image: DecorationImage(
+                image: AssetImage('images/my_image1.jpg')
+              )
+            ),
+        )
+      )
+      )
+    );
+  }
+}

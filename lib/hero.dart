ddaf3c31ca76b20ca4072a01ff6bac99d3d4875c
emilykuo <emilykuo@cw.com.tw>
2019-10-04 18:13:06 +0800

    hero

--- lib/hero.dart
+++ lib/hero.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter/scheduler.dart';
 void main(){
   runApp(new MyApp());
 } 
@@ -9,41 +9,112 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(  
       title: 'hello',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Succulent'),
-          backgroundColor: Color.fromRGBO(250, 205, 75, 1.0),
-        ),
-        body: (
-          Hero(
-            tag: 'succulent',
-            child: Container(
-            decoration: BoxDecoration(
-              color: Colors.white,
-              image: DecorationImage(
-                image: AssetImage('assets/images/succulent.jpg')
-              )
+      home: MyHero()
+    );
+  }
+}
+class MyHero extends StatelessWidget {
+  Widget build(BuildContext context) {
+    timeDilation = 1.0; // 1.0 means normal animation speed.
+    var screenWidth = MediaQuery.of(context).size.width;
+    var rowWidth = screenWidth-24;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Hero List'),
+      ),
+      body: Container(  //list頁，push推入換頁
+        width: screenWidth,
+        padding: EdgeInsets.all(8.0),
+        color: Colors.yellow[50],
+        child: Row(
+          children: [
+            Align(
+              alignment: Alignment.topLeft,
+              child: PhotoHero(
+              photo: 'assets/images/succulent.jpg',
+              width: rowWidth/3,
+              onTap: () {
+                Navigator.of(context).push(MaterialPageRoute<void>( 
+                  builder: (BuildContext context) {
+                    return Scaffold(
+                      appBar: AppBar(
+                        title: const Text('Flippers Page'),
+                      ),
+                      body: Center(
+                        child: PhotoHero(
+                          photo: 'assets/images/succulent.jpg',
+                          width: 300.0,
+                          onTap: () {
+                            Navigator.of(context).pop(); 
+                          },
+                        ),
+                      ),
+                    );
+                  }
+                ));
+              },
             ),
-          )
-        )
+          ),
+          // SizedBox(width: 8,),
+          // Align(
+          //   alignment: Alignment.topRight,
+          //   child: PhotoHero(
+          //   photo: 'assets/images/succulent.jpg',
+          //   width: rowWidth/3*2,
+          //   onTap: () {
+          //     Navigator.of(context).push(MaterialPageRoute<void>( 
+          //       builder: (BuildContext context) {
+          //         return Scaffold(
+          //           appBar: AppBar(
+          //             title: const Text('Flippers Page'),
+          //           ),
+          //           body: Center(
+          //             child: PhotoHero(
+          //               photo: 'assets/images/succulent.jpg',
+          //               width: 300.0,
+          //               onTap: () {
+          //                 Navigator.of(context).pop(); 
+          //               },
+          //             ),
+          //           ),
+          //         );
+          //       }
+          //     ));
+          //   },
+          // ),
+          // )
+        ]
           
+        ),
       )
-      )
+      
     );
   }
 }
 
-class MyDetail extends StatelessWidget {
-  @override
-  Widget build(BuildContext context){
-    return Hero(
-      tag: 'succulent',
-      child: Image.asset('assets/images/succulent.jpg')
+class PhotoHero extends StatelessWidget {
+  const PhotoHero({ Key key, this.photo, this.onTap, this.width }) : super(key: key);
+
+  final String photo;
+  final VoidCallback onTap;
+  final double width;
+
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: width,
+      child: Hero(
+        tag: photo,
+        child: Material(
+          color: Colors.transparent,
+          child: InkWell(
+            onTap: onTap,
+            child: Image.asset(
+              photo,
+              fit: BoxFit.contain,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
-
-//--pubspec.yaml--
-// flutter:
-//   assets:
-//     - images/

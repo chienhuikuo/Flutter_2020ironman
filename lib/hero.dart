b7f00923b31257a7f27fe969290d5f011be393f3
emilykuo <emilykuo@cw.com.tw>
2019-10-05 17:49:12 +0800

    hero

--- lib/hero.dart
+++ lib/hero.dart
@@ -18,6 +18,7 @@ class MyHero extends StatelessWidget {
     timeDilation = 1.0; // 1.0 means normal animation speed.
     var screenWidth = MediaQuery.of(context).size.width;
     var rowWidth = screenWidth-24;
+    
     return Scaffold(
       appBar: AppBar(
         title: const Text('Hero List'),
@@ -31,18 +32,19 @@ class MyHero extends StatelessWidget {
             Align(
               alignment: Alignment.topLeft,
               child: PhotoHero(
-              photo: 'assets/images/succulent.jpg',
+              photo: 'assets/images/succulent2.jpg',
               width: rowWidth/3,
               onTap: () {
                 Navigator.of(context).push(MaterialPageRoute<void>( 
                   builder: (BuildContext context) {
                     return Scaffold(
                       appBar: AppBar(
-                        title: const Text('Flippers Page'),
+                        title: const Text('Hero Page'),
+                        backgroundColor: Colors.lightGreen,
                       ),
                       body: Center(
                         child: PhotoHero(
-                          photo: 'assets/images/succulent.jpg',
+                          photo: 'assets/images/succulent2.jpg',
                           width: 300.0,
                           onTap: () {
                             Navigator.of(context).pop(); 
@@ -55,34 +57,36 @@ class MyHero extends StatelessWidget {
               },
             ),
           ),
-          // SizedBox(width: 8,),
-          // Align(
-          //   alignment: Alignment.topRight,
-          //   child: PhotoHero(
-          //   photo: 'assets/images/succulent.jpg',
-          //   width: rowWidth/3*2,
-          //   onTap: () {
-          //     Navigator.of(context).push(MaterialPageRoute<void>( 
-          //       builder: (BuildContext context) {
-          //         return Scaffold(
-          //           appBar: AppBar(
-          //             title: const Text('Flippers Page'),
-          //           ),
-          //           body: Center(
-          //             child: PhotoHero(
-          //               photo: 'assets/images/succulent.jpg',
-          //               width: 300.0,
-          //               onTap: () {
-          //                 Navigator.of(context).pop(); 
-          //               },
-          //             ),
-          //           ),
-          //         );
-          //       }
-          //     ));
-          //   },
-          // ),
-          // )
+          SizedBox(width: 8,),
+          Align(
+            alignment: Alignment.topRight,
+            child: PhotoHero(
+            photo: 'assets/images/succulent.jpg',
+            width: rowWidth/3*2,
+            onTap: () {
+              Navigator.of(context).push(MaterialPageRoute<void>( 
+                builder: (BuildContext context) {
+                  return Scaffold(
+                    appBar: AppBar(
+                      title: const Text('Hero Page'),
+                      backgroundColor: Colors.lightGreen,
+                    ),
+                    body: Center(
+                      child: PhotoHero(
+                        photo: 'assets/images/succulent.jpg',
+                        width: 300.0,
+                        onTap: () {
+                          Navigator.of(context).pop(); 
+                        },
+                      ),
+                    ),
+                    backgroundColor: Colors.yellow[50],
+                  );
+                }
+              ));
+            },
+          ),
+          )
         ]
           
         ),

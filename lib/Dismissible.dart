44a6ad7a474e2dc8502f05eccc0c6f56ddce060d
emilykuo <emilykuo@cw.com.tw>
2019-10-07 11:22:52 +0800

    dismissible

--- lib/Dismissible.dart
+++ lib/Dismissible.dart
@@ -10,7 +10,7 @@ class MyApp extends StatefulWidget {
 }
 
 class MyAppState extends State<MyApp> {
-  final items = List<String>.generate(20, (i) => "Item ${i + 1}"); //生成list
+  final items = List<String>.generate(20, (i) => "Item ${i + 1}"); //創多筆列表資料
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -19,21 +19,25 @@ class MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: Text('slide remove'),
         ),
-        body: ListView.builder(
+        body: ListView.builder(  //渲染長列表
           itemCount: items.length,
           itemBuilder: (context, index) {
             final item = items[index];
 
             return Dismissible(
-              key: Key(item),
-              onDismissed: (direction) {
-                setState(() {
+              key: Key(item),   //每一個Dismissible都必須有專屬的key，讓Flutter能夠辨識
+              onDismissed: (direction) {   //項目移除後要做什麼事
+                setState(() {   //從data中移除項目
                   items.removeAt(index);
                 });
-                Scaffold.of(context)
-                    .showSnackBar(SnackBar(content: Text("$item dismissed")));
+                Scaffold.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text("$item 掰掰"),
+                    duration: Duration(milliseconds: 500), //預設四秒有點太慢了
+                  )
+                );  //顯示Snackbar
               },
-              background: Container(color: Colors.red),
+              background: Container(color: Colors.red), //滑動時跑出來的背景色
               child: ListTile(title: Text('$item')),
             );
           }
